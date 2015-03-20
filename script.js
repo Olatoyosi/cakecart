f68dcf6f115f592d5a5cc19c8559e4f26158f76e
Olatoyosi <khadeejah005@gmail.com>
2015-03-20 07:37:15 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,73 @@
+
+$('document').ready(function(){
+	var prrice=0;
+	var productArr = [];
+
+	$('.list li').draggable({
+		appendTo:'body',
+		helper: 'clone'
+	});
+
+	function countInArray(productArr, productid) {
+		var count = 0;
+			 for (var i = 0; i < productArr.length; i++) {
+	        if (productArr[i] === productid) {
+	            count++;
+	        }
+	    }
+		return count;
+	}
+
+
+	$('#cartspace ol').droppable({
+		drop: function(event, ui){
+			var productid = ui.draggable.attr("data-price");
+			productArr.push(parseInt(productid));
+			var total = 0;
+			for(i in productArr)
+			{
+				 total += productArr[i];
+			}
+
+			var index = countInArray(productArr, parseInt(productid));
+			var self = $(this);
+			if (self.find("[data-price=" + productid + "]").length){
+				$('#total').empty();
+				$('#total').append(total);
+				return;
+			};
+
+		 	$("<li data-price=" + productid + " >" 
+		 			+ ui.draggable.text() + index + "<button>Delete</button> </li>")
+		 				.appendTo(this);
+			 	$('#total').empty();
+			 	$('#total').append(total);
+			}
+	});
+		$('#cancel').click(function(){
+		$('#droplist').empty();
+		$('#total').empty();
+		});
+
+		$('#complete').click(function()
+		{
+		$('#text').append("<textarea>enter your name, number and address:</textarea>");
+		});
+
+		$('#sender').click(function(){
+		$('.sendmsg').append("Your Order has been received.You would receive a mail soonest");
+		});
+
+		$('#senddetails').click(function(){
+		$('.details').append("Your Order has been received.You would receive a mail soonest");
+		});
+
+		$('button').click(function(){
+			$('#droplist li').remove();
+			total -=productid;
+			console.log(total);
+			$('#total').empty();
+			$('#total').append(total);
+		});
+
+});
